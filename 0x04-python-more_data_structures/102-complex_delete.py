095f5f15db9b7cb08bfee04b129507d3e9146fd5
Alicenyamburanjeri <alicenyamburanjeri@gmail.com>
2022-12-08 21:51:14 +0300

    data str

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def complex_delete(a_dictionary, value):
     list_keys = list(a_dictionary.keys())
 
@@ -6,4 +7,4 @@ def complex_delete(a_dictionary, value):
         if value == a_dictionary.get(value_dic):
             del a_dictionary[value_dic]
 
-            return (a_dictionary)
+    return (a_dictionary)
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def to_subtract(list_num):
     to_sub = 0
     max_list = max(list_num)
@@ -38,4 +39,4 @@ def to_subtract(list_num):
 
                             num += to_subtract(list_num)
 
-                            return (num)
+            return (num)
