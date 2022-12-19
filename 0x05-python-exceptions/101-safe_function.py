714efef6a9741813ed77bdc19ccec12f4f15e713
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2022-12-19 08:20:26 -0800

    exception

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,6 +7,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except:
+    except Exception:
         print("Exception: {}".format(sys.exc_info()[i]), file=sys.stderr)
         return (None)
