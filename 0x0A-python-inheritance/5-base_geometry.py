7e2d769f2f4c8a037f283d73d0a269406870d666
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2023-01-11 02:48:11 -0800

    inheritance

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Defines an emty class BaseGeometry """
+
+
+Class BaseGeometry:
+    """ Represents base geometry """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+""" Defines a BaseGeometry class """
+
+
+Class BaseGeometry:
+    """ Represents base geometry """
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
