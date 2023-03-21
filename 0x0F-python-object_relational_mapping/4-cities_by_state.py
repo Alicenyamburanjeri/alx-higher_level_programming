80c5e11ba374cb680418373d38a85ee9383e8f9e
Alicenyamburanjeri <alicenyamburanjeri@gmail.com>
2023-03-21 10:41:23 +0000

    ORM

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
 """
-A script lists all cities from
-the database `hbtn_0e_4_usa`.
+A script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+The script should be safe from
+MySQL injections!
 """
 
 import MySQLdb as db
 from sys import argv
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     db_connect = db.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
 
-    with db_connect.cursor() as db_cursor:
-        db_cursor.execute("SELECT cities.id, cities.name, states.name \
-                                FROM cities JOIN states ON cities.state_id \
-                                = states.id ORDER BY cities.id ASC")
-        rows_selected = db_cursor.fetchall()
+    db_cursor = db_connect.cursor()
+    db_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE \
+                    BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
 
-    if rows_selected is not None:
-        for row in rows_selected:
-            print(row)
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
+        print(row)
