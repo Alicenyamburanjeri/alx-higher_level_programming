68738136f13800bebb855f7d4d7488551ff06f19
Alicenyamburanjeri <alicenyamburanjeri@gmail.com>
2023-03-21 10:27:09 +0000

    ORM

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 """
-A script that  takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument and is safe from MySQL injections!
-Your script should take 4 arguments: mysql username, mysql password, database name and state name searched (safe from MySQL injection)
-You must use the module MySQLdb (import MySQLdb)
-Your script should connect to a MySQL server running on localhost at port 3306
-Results must be sorted in ascending order by states.id
+A script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument
+should be safe from MySQL injections
 """
-
-import MySQLdb
 import sys
+import MySQLdb
 
 
 if __name__ == '__main__':
