0f3187c1febcd03ac7faa14fcd4e452552e24410
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2023-04-04 03:18:08 -0700

    python network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 # A script that takes in a URL
 # sends a request to that URL
-=======
-# A Bash script that takes in a URL,
-# sends a request to that URL,
->>>>>>> 05b464001dc49c44344b4fa0851554180662195f
-=======
-# A Bash script that takes in a URL,
-# sends a request to that URL,
->>>>>>> 05b464001dc49c44344b4fa0851554180662195f
-=======
-# A Bash script that takes in a URL,
-# sends a request to that URL,
->>>>>>> 05b464001dc49c44344b4fa0851554180662195f
-# and displays the size of the body of the response
 curl -s "$1" | wc -c
