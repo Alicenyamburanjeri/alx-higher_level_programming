ac42c01cef8f6015672a2d82b208fa242dcdc267
Alicenyamburanjeri <alicenyamburanjeri@gmail.com>
2023-04-04 13:31:49 -0700

    py networks

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays only the status code of the response
-curl -s -o /dev/null -w "%(http_code)" "$1"
+# A bash script that displays only the status code of the response
+curl -sL -o /dev/null -w "%http_code" "$1"
