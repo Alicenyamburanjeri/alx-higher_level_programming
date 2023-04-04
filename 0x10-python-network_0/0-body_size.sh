51e58c6ae1dced8a45f287039963b23d2d1a37fe
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2023-04-04 03:05:10 -0700

    resolving

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+<<<<<<< HEAD
 # A script that takes in a URL
 # sends a request to that URL
+=======
+# A Bash script that takes in a URL,
+# sends a request to that URL,
+>>>>>>> 05b464001dc49c44344b4fa0851554180662195f
 # and displays the size of the body of the response
 curl -s "$1" | wc -c
--- 0x10-python-network_0/README.md
+++ 0x10-python-network_0/README.md
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 Python Network
+=======
+python networking
+>>>>>>> 05b464001dc49c44344b4fa0851554180662195f
